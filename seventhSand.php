5abdf2189ba03dee60d0d6239030e8a82e5f439c
Daniel seventhSand <zoidsimangunsong@gmail.com>
2015-03-24 09:46:30 +0700

    Autoloader function

---
+++ seventhSand.php
@@ -0,0 +1,18 @@
+<?php
+ 
+/**
+ * @Author 		Daniel Simangunsong
+ * @Company		~
+ * @copyright	2015
+ * @Package	    
+ * @Module      
+ * @License		GNU 2 License
+ * 
+ * Calm seas, never make skillfull sailors
+ * Include this file when you need the package, and register seventhSand method as an autoloader to make sure everything working smothly	
+**/ 
+
+function seventhSand()
+{
+  
+}
